11417f15a8ce8fc8dfbca3a917809df290ea6fdf
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-08-09 18:35:24 +0200

    project from initializr

---
+++ src/main/java/com/example/demo/SpringBatch5NativeImagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch5NativeImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch5NativeImagesApplication.class, args);
+	}
+
+}
